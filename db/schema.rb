4f139d2aafa5bdd007decf3105becebacd55cd97
ShinobiWarior <a.ivezic@yahoo.com>
2022-01-17 20:42:08 +0100

    Create devise User model with token_authenticatable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_13_234022) do
+ActiveRecord::Schema.define(version: 2022_01_17_190741) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -45,11 +45,20 @@ ActiveRecord::Schema.define(version: 2022_01_13_234022) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "email", default: "", null: false
     t.string "image"
     t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "authentication_token"
+    t.datetime "authentication_token_created_at", precision: 6
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at", precision: 6
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "questions", "categories"
